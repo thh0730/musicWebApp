4b67dd6cb284c253a147bc4962de26977cfc8534
Shoutting <50048135+thh0730@users.noreply.github.com>
2020-04-14 21:03:53 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -20,6 +20,7 @@ module.exports = {
     proxy: {
       '/api': { // 使用"/api"来代替"http://f.apiplus.c"
         target: 'http://127.0.0.1:3000', // 源地址
+       // target: "http://music.it666.com:3666", // 源地址
         changeOrigin: true, // 改变源
         pathRewrite: {
           '^/api': 'http://127.0.0.1:3000' // 路径重写
